6a82e4cad49c75b758cc1a2ce2ac9dffd3a0c95f
Parjal Rai <parjal.rai@dvla.gov.uk>
2025-04-14 16:29:51 +0100

    add customerdtoservice with tests

--- src/test/java/service/CustomerDTOServiceTest.java
+++ src/test/java/service/CustomerDTOServiceTest.java
@@ -4,6 +4,7 @@ import org.example.dto.CustomerDTO;
 import org.example.model.Customer;
 import org.example.repository.CustomerRepository;
 import org.example.service.CustomerDTOService;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,8 +12,20 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class CustomerDTOServiceTest {
@@ -35,16 +48,133 @@ public class CustomerDTOServiceTest {
     public void test_convertToCustomerDTO(){
         Customer customer = new Customer(1, "user123", "password12", "John","Doe");
 
-        CustomerDTO actualDTO  = customerDTOService.convertToCustomerDTO(customer);
+        CustomerDTO actualDTO  = CustomerDTOService.convertToCustomerDTO(customer);
 
         assertEquals(1, actualDTO.getCustomerId());
         assertEquals("user123", actualDTO.getUsername());
         assertEquals("password12", actualDTO.getPassword());
         assertEquals("John", actualDTO.getFirstName());
         assertEquals("Doe", actualDTO.getLastName());
+    }
 
+    @Test
+    public void test_getAllCustomerDTO(){
+        List<Customer> customerList = Arrays.asList(
+                new Customer(1, "user123", "password12", "John","Doe"),
+                new Customer(2,"user234", "password21", "Jane", "Doe")
+        );
+        Pageable pageable = PageRequest.of(0,10);
+        Page<Customer> customerPage = new PageImpl<>(customerList, pageable, customerList.size());
+
+        when(customerRepository.findAll(pageable)).thenReturn(customerPage);
+
+        Page<CustomerDTO> actualPage = customerDTOService.findAllCustomers(0, 10);
+        List<CustomerDTO> actualList = actualPage.getContent();
+
+        assertEquals(2, actualList.size());
+        assertEquals(1, actualList.get(0).getCustomerId());
+        assertEquals(2, actualList.get(1).getCustomerId());
 
     }
+
+    @Test
+    public void test_getCustomerDTOById(){
+        Customer customer = new Customer(1, "user123", "password12", "John","Doe");
+        when(customerRepository.findById(1)).thenReturn(Optional.of(customer));
+
+        CustomerDTO customerDTO = customerDTOService.findCustomerById(1);
+
+        assertEquals("John", customerDTO.getFirstName());
+        assertEquals("Doe", customerDTO.getLastName());
+    }
+
+    @Test
+    public void test_addCustomer(){
+        Customer customer = new Customer(1, "user123", "password12", "John","Doe");
+        when(customerRepository.save(any(Customer.class))).thenReturn(customer);
+
+        CustomerDTO actualDTO = customerDTOService.addCustomer(customer);
+
+        assertEquals(1, actualDTO.getCustomerId());
+    }
+
+    @Test
+    public void test_replaceCustomer(){
+        Customer customer = new Customer(1, "user123", "password12", "John","Doe");
+        Customer expectCustomer =  new Customer(1,"user234", "password21", "Jane", "Dane");
+
+
+        when(customerRepository.findById(1)).thenReturn(Optional.of(customer));
+
+        CustomerDTO actualCustomer = customerDTOService.replaceCustomer(1, expectCustomer);
+
+        assertEquals("Jane", actualCustomer.getFirstName());
+        assertEquals("Dane", actualCustomer.getLastName());
+        assertEquals("user234", actualCustomer.getUsername());
+        assertEquals("password21", actualCustomer.getPassword());
+    }
+    @Test
+    public void test_replaceCustomer_throwRuntimeException(){
+        Customer expectedCustomer = new Customer();
+        when(customerRepository.findById(1)).thenThrow(new RuntimeException("Customer not found"));
+
+        assertThrows(RuntimeException.class, () -> {
+            customerDTOService.replaceCustomer(1,expectedCustomer);
+        });
+
+        verify(customerRepository).findById(1);
+    }
+
+
+    @Test
+    public void test_updateCustomer_userFound(){
+        Customer customer = new Customer(1, "user123", "password12", "John","Doe");
+
+        Customer expectCustomer = new Customer();
+        expectCustomer.setFirstName("Jane");
+        expectCustomer.setLastName("Dane");
+        expectCustomer.setUsername("user234");
+        expectCustomer.setPassword("password21");
+
+        when(customerRepository.findById(1)).thenReturn(Optional.of(customer));
+
+        CustomerDTO actualCustomer = customerDTOService.updateCustomer(1, expectCustomer);
+
+        assertEquals("Jane", actualCustomer.getFirstName());
+        assertEquals("Dane", actualCustomer.getLastName());
+        assertEquals("user234", actualCustomer.getUsername());
+        assertEquals("password21", actualCustomer.getPassword());
+
+    }
+    
+    @Test
+    public void test_updateCustomer_throwRuntimeException(){
+        Customer expectedCustomer = new Customer();
+        when(customerRepository.findById(1)).thenThrow(new RuntimeException("Customer not found"));
+
+        assertThrows(RuntimeException.class, () -> {
+            customerDTOService.updateCustomer(1,expectedCustomer);
+        });
+
+        verify(customerRepository).findById(1);
+    }
+
+    @Test
+    public void test_deleteCustomer_returnTrueAndFalse(){
+        Customer customer = new Customer(1, "user123", "password12", "John","Doe");
+
+        when(customerRepository.findById(1)).thenReturn(Optional.of(customer));
+
+        boolean result = customerDTOService.deleteCustomer(1);
+        boolean resultFalse = customerDTOService.deleteCustomer(2);
+
+        assertTrue(result);
+        assertFalse(resultFalse);
+    }
+
+
+
+
 
 
 }
