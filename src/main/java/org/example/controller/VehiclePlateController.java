cf7d44ed94882e43f0db7c61ddcdb534a648587f
Parjal Rai <parjal.rai@dvla.gov.uk>
2025-04-16 17:54:34 +0100

    add getAllVehiclePlatesByReg method

---
+++ src/main/java/org/example/controller/VehiclePlateController.java
@@ -0,0 +1,37 @@
+package org.example.controller;
+
+import org.example.dto.VehiclePlateDTO;
+import org.example.service.VehiclePlateDTOService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VehiclePlateController {
+
+    @Autowired
+    private VehiclePlateDTOService vehiclePlateDTOService;
+
+    @GetMapping("/v1/vehiclePlates")
+    public ResponseEntity<Page<VehiclePlateDTO>> getAllVehiclePlates(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        Page<VehiclePlateDTO> result = vehiclePlateDTOService.findAllVehiclePlates(page, size);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/v1/vehiclePlates/reg/{reg}")
+    public ResponseEntity<Page<VehiclePlateDTO>> getAllVehiclePlatesByReg(@PathVariable String reg,
+                                                                          @RequestParam(defaultValue = "0") int page,
+                                                                          @RequestParam(defaultValue = "10") int size){
+        Page<VehiclePlateDTO> result = vehiclePlateDTOService.findAllVehiclePlatesByReg(reg, page, size);
+        return ResponseEntity.ok(result);
+    }
+
+
+}
---
+++ src/test/java/controller/VehiclePlateControllerTest.java
@@ -0,0 +1,61 @@
+package controller;
+
+import org.example.controller.VehiclePlateController;
+import org.example.dto.VehiclePlateDTO;
+import org.example.service.VehiclePlateDTOService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class VehiclePlateControllerTest {
+    @InjectMocks
+    private VehiclePlateController vehiclePlateController;
+    @Mock
+    private VehiclePlateDTOService vehiclePlateDTOService;
+
+    @Test
+    public void test_getAllVehiclePlates() {
+        List<VehiclePlateDTO> vehiclePlateDTOList = Arrays.asList(
+                new VehiclePlateDTO(1, "sa12uvw", false, false, 20.30, 1),
+        new VehiclePlateDTO(2, "uk56xrw", false, false, 60.30, 1)
+        );
+        Page<VehiclePlateDTO> page = new PageImpl<>(vehiclePlateDTOList);
+        when(vehiclePlateDTOService.findAllVehiclePlates(0,10)).thenReturn(page);
+
+        ResponseEntity<Page<VehiclePlateDTO>> actual = vehiclePlateController.getAllVehiclePlates(0, 10);
+
+        assertNotNull(actual);
+        assertEquals(HttpStatus.OK, actual.getStatusCode());
+    }
+
+    @Test
+    public void test_getAllVehiclePlatesByReg(){
+        List<VehiclePlateDTO> vehiclePlateDTOList = Arrays.asList(
+                new VehiclePlateDTO(1, "sa12uvw", false, false, 20.30, 1),
+                new VehiclePlateDTO(2, "sa56xrw", false, false, 60.30, 1)
+        );
+        Page<VehiclePlateDTO> page = new PageImpl<>(vehiclePlateDTOList);
+        when(vehiclePlateDTOService.findAllVehiclePlatesByReg("s",0,10)).thenReturn(page);
+
+        ResponseEntity<Page<VehiclePlateDTO>> actual = vehiclePlateController.getAllVehiclePlatesByReg("s",0,10);
+
+        assertNotNull(actual);
+        assertEquals(HttpStatus.OK, actual.getStatusCode());
+        assertEquals(page, actual.getBody());
+    }
+
+}
