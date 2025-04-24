1cdbb4be34ce30d31ab21d563a17abfa70a10fa9
Parjal Rai <parjal.rai@dvla.gov.uk>
2025-04-24 17:37:24 +0100

    create repository

---
+++ src/main/java/org/example/repository/BalanceRepository.java
@@ -0,0 +1,9 @@
+package org.example.repository;
+
+import org.example.model.Balance;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BalanceRepository extends JpaRepository<Balance, Integer> {
+}
