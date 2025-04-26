824a1dd98c4a5bde4c3361c3fe41d5c6c0955170
Parjal Rai <parjal.rai@dvla.gov.uk>
2025-04-26 17:45:39 +0100

    create admin class

--- src/main/java/org/example/security/SecurityConfig.java
+++ src/main/java/org/example/security/SecurityConfig.java
@@ -1,29 +1,91 @@
-package org.example.security;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeHttpRequests()
-                .anyRequest().authenticated();
-        http.oauth2ResourceServer()
-                .jwt();
-
-        http.sessionManagement()
-                .sessionCreationPolicy(STATELESS);
-        return http.build();
-    }
-}
+//package org.example.security;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.convert.converter.Converter;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.authentication.AbstractAuthenticationToken;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.oauth2.jwt.Jwt;
+//import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//import java.util.*;
+//import java.util.stream.Collectors;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain resourceServerSecurityFilterChain(HttpSecurity http, Converter<Jwt, AbstractAuthenticationToken> authenticationConverter) throws Exception {
+//        http.oauth2ResourceServer(resourceServer ->
+//                resourceServer.jwt(jwt -> jwt.jwtAuthenticationConverter(authenticationConverter))
+//        );
+//
+//        http.sessionManagement(session ->
+//                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//        ).csrf(csrf ->
+//                csrf.disable()
+//        );
+//
+//        http.authorizeHttpRequests(requests ->
+//                requests
+//                        .requestMatchers(HttpMethod.GET, "/**").hasAnyRole("CUSTOMER", "ADMIN")
+//                        .requestMatchers(HttpMethod.POST, "/**").hasRole("ADMIN")
+//                        .requestMatchers(HttpMethod.PUT, "/**").hasRole("ADMIN")
+//                        .requestMatchers(HttpMethod.PATCH, "/**").hasAnyRole("CUSTOMER", "ADMIN")
+//                        .requestMatchers(HttpMethod.DELETE, "/**").hasRole("ADMIN")
+//                        .anyRequest().authenticated()
+//        );
+//
+//        return http.build();
+//    }
+//
+//    @Bean
+//    JwtAuthenticationConverter authenticationConverter(
+//            Converter<Map<String, Object>, Collection<GrantedAuthority>> authoritiesConverter) {
+//        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+//        converter.setJwtGrantedAuthoritiesConverter(jwt ->
+//                authoritiesConverter.convert(jwt.getClaims())
+//        );
+//        return converter;
+//    }
+//
+//    @Bean
+//    AuthoritiesConverter realmRolesAuthoritiesConverter() {
+//        return claims -> {
+//            Map<String, Object> resourceAccess = (Map<String, Object>) claims.get("resource_access");
+//            if (resourceAccess == null) return Collections.emptyList();
+//
+//            Map<String, Object> clientAccess = (Map<String, Object>) resourceAccess.get("buymyplate-rest-api");
+//            if (clientAccess == null) return Collections.emptyList();
+//
+//            List<String> roles = (List<String>) clientAccess.get("roles");
+//            if (roles == null) return Collections.emptyList();
+//
+//            return roles.stream()
+//                    .map(role -> "ROLE_" + role) // Add prefix and standardize case
+//                    .map(SimpleGrantedAuthority::new)
+//                    .collect(Collectors.toList());
+//        };
+//    }
+//}
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http.csrf().disable()
+//                .authorizeHttpRequests()
+//                .anyRequest().authenticated();
+//        http.oauth2ResourceServer()
+//                .jwt();
+//
+//        http.sessionManagement()
+//                .sessionCreationPolicy(STATELESS);
+//        return http.build();
+//    }
+//}
 
